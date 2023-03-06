e2b58b39631d8c823ca0921d85757a5272de6658
Andras Bacsai <andras.bacsai@gmail.com>
2023-03-06 21:58:20 +0100

    first commit

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Layout from "./Layout.svelte";
+</script>
+
+<svelte:head>
+    <title>Welcome</title>
+</svelte:head>
+Home
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+    plugins: [
+        laravel.default({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        svelte(),
+    ],
+    optimizeDeps: {
+        include: [
+            "@inertiajs/inertia",
+            "@inertiajs/inertia-svelte",
+        ]
+    }
+});
